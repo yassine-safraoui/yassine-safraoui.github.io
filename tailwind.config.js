00c28834d57979d1499bbdd893af8cfc1c412a00
Yassine Safraoui <yassine.safraoui@grenoble-inp.org>
2025-08-12 20:00:51 +0200

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,19 @@ const typography = require('@tailwindcss/typography');
 module.exports = {
 	content: ['./hugo_stats.json'],
 	plugins: [typography],
+	important: true,
+	darkMode: "class",
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--color-background)",
+				"background-secondary": "var(--color-background-secondary)",
+				text: "var(--color-text)",
+				"text-secondary": "var(--color-text-secondary)",
+				link: "var(--color-link)",
+				primary: "var(--color-primary)",
+				secondary: "var(--color-secondary)"
+			}
+		}
+	}
 };
