790b512ff57fdec7ba49ddcbd5112c56ef0668f7
Yassine Safraoui <yassine.safraoui@grenoble-inp.org>
2026-02-07 20:50:30 +0100

    eslint + prettier setup

---
+++ eslint.config.mjs
@@ -0,0 +1,87 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import astro from "eslint-plugin-astro";
+import prettier from "eslint-plugin-prettier";
+import eslintPluginBetterTailwindcss from "eslint-plugin-better-tailwindcss";
+
+
+// parsers
+const tsParser = tseslint.parser;
+const astroParser = astro.parser;
+
+export default defineConfig([
+  // Global configuration
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+
+  // Base configs
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    // enable all recommended rules
+    extends: [
+      eslintPluginBetterTailwindcss.configs.recommended
+    ],
+
+    // if needed, override rules to configure them individually
+    // rules: {
+    //   "better-tailwindcss/enforce-consistent-line-wrapping": ["warn", { printWidth: 100 }]
+    // },
+
+    settings: {
+      "better-tailwindcss": {
+        // tailwindcss 4: the path to the entry file of the css based tailwind config (eg: `src/global.css`)
+        entryPoint: "src/styles/global.css",
+        // tailwindcss 3: the path to the tailwind config file (eg: `tailwind.config.js`)
+        tailwindConfig: "tailwind.config.js"
+      }
+    },
+
+    files: ["**/*.astro"],
+  },
+
+  // Prettier config
+  {
+    plugins: {
+      prettier: prettier,
+    },
+    rules: {
+      // disable warnings, since prettier should format on save
+      "prettier/prettier": "off",
+    },
+  },
+
+  // astro setup with a11y
+  astro.configs.recommended,
+  astro.configs["jsx-a11y-recommended"],
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: astroParser,
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: [".astro"],
+        sourceType: "module",
+        ecmaVersion: "latest",
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      "no-undef": "off", // Disable "not defined" errors for specific Astro types that are globally available (ImageMetadata)
+      "@typescript-eslint/no-explicit-any": "off", // you may want this as it can get annoying
+    },
+  },
+
+  // Ignore patterns
+  {
+    ignores: ["dist/**", "**/*.d.ts", ".github/", "old_portfolio/**"],
+  },
+]);
