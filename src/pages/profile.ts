90c55efb7ca83e8f2fbec8825679f79a0e2bcacc
Yassine Safraoui <yassine.safraoui@grenoble-inp.org>
2026-02-12 16:00:23 +0100

    better error handling for non existing profile

--- src/pages/profile.ts
+++ src/pages/profile.ts
@@ -1,9 +1,10 @@
 import { getEntry } from "astro:content";
+
 const entry = await getEntry("profile", "main");
-if (entry == undefined) {
-  console.error("Couldn't read profile");
-  throw new Error("Couldn't read profile. Panicing...");
+if (entry === undefined) {
+	console.error("Couldn't read profile");
+	throw new Error("Couldn't read profile. Panicing...");
 }
 
-const profile = entry!.data;
+const profile = entry.data;
 export default profile;
